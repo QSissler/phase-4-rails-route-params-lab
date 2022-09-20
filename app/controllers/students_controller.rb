e20e1d2582ca4bdec1002e1cecdd9c72d931337a
Quinn Sissler <QSissler@gmail.com>
2022-09-20 11:31:17 -0400

    commit tree 964dab0002890f443cfe92f2ad9a44db1d68c003

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,27 @@
 class StudentsController < ApplicationController
 
+  # def index
+  #   students = if params[:name]
+  #                Student.by_name(params[:name])
+  #              else
+  #                Student.all
+  #              end    
+  #   render json: students
+  # end
+
   def index
-    students = Student.all
+    if params[:name]
+      students = Student.select{|student| student.first_name.downcase == params[:name] || student.last_name.downcase == params[:name]}
+    else
+      students = Student.all
+    end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 end
+
